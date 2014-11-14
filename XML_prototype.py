51440edd776a14957fce8a90ed09b6e5b005d211
Rodney <rodneynyanh@gmail.com>
2014-11-14 03:43:52 -0800

    New files LRG files have been added plus a few bit and bobs

--- XML_prototype.py
+++ XML_prototype.py
@@ -2,15 +2,14 @@
 import sys
 import xml.etree.ElementTree as etree
 import glob
-
-
-#Testing 
-
+import re
 
 #Read input arguments
 fileName = sys.argv[1]
 #Check file name is valid/ file is present
-assert fileName[-4:] == '.xml', 'You have the wrong input file'   
+assert fileName[-4:] == '.xml', 'You have the wrong input file' 
+
+  
 #fileOutTitle = sys.argv[2]
 #fileOut = open(fileOutTitle, 'w')
 
@@ -25,6 +24,10 @@ root = tree.getroot()
 #fileexist = glob.glob(sys.argv[2])
 #if len(fileexist) > 0:
 	#WARN about file overwrite
+
+#Check the version of the file we are openeing is correct
+assert root.attrib['schema_version'] == '1.8', 'This file is not the correct version'
+
 out = open('output',"a")
 
 fixannot = root.find('fixed_annotation')
@@ -37,6 +40,13 @@ for element in fixannot.iter():
 		genseq = max(sequences, key=len)
 		protseq = min(sequences,key=len)
 		
+
+nucleotides = ['A','T','C','G']
+
+for l in genseq:
+	if l not in nucleotides:
+		print "this is not the genomic sequence"
+		exit()
 		
 
 print protseq
@@ -47,12 +57,20 @@ for items in fixannot.iter(tag="transcript"):
         if items.attrib['name'] == "t1":
             exons = items.iter('exon')
 
+##REGEX
+'''regex = re.compile('LRG_*')
+for exon in exons:
+	for coordinates in exon:
+		print coordinates.attrib['coord_system']
+		if re.match(regex, coordinates.attrib['coord_system']):
+			print  coordinates.attrib['coord_system']'''
+
 
 for exon in exons:
-	exonNumber = exon.attrib['label']
-#	print exon    
+	exonNumber = exon.attrib['label']  
 	for coordinates in exon:
-		if coordinates.attrib['coord_system'] == 'LRG_1':
+		if coordinates.attrib['coord_system'][-2] not in ['t','p']:
+			print coordinates.attrib['coord_system']
 			startIndex = int(coordinates.attrib['start'])
 			endIndex = int(coordinates.attrib['end'])
 			exonLength = endIndex - startIndex
